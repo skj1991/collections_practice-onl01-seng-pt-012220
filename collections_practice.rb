6f84c2db56a9fe9166e1774dc20604c5cc1117ac
Stacy Johns <stacykjohns@icloud.com>
2020-01-31 01:35:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by do |word|
+    word.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |word|
+    word[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum += n
+  end
+end
+
+def add_s(array)
+  array.each do |word|
+    if word != array[1]
+      word << "s"
+    end
+  end
+end
